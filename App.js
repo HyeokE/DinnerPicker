07fa6d21a7f84e26ab2d69cd40dbc3a7e2ffd0d6
HYEOK_E <jhjeong00@gmail.com>
2021-11-06 21:30:28 +0900

    feat : project setting, navigation 추가

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import Home from "./src/pages/Home";
+import Setting from "./src/pages/Setting";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        activeColor="black"
+        barStyle={{ backgroundColor: "#f9fafb" }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Setting}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="table-settings"
+                color={color}
+                size={26}
+              />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Setting.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from "react-native";
+
+const Setting = () => {
+    return (
+        <SafeAreaView >
+        <View>
+            <Text>
+                setting
+            </Text>
+        </View>
+        </SafeAreaView>
+    );
+};
+
+export default Setting;
